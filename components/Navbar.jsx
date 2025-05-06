51f310f4af3a8c7c11f748a926113897fbf60139
sathish R <116962209+Sathish-R08@users.noreply.github.com>
2025-05-06 17:48:24 +0530

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const { openSignIn } = useClerk()
 
   return (
-    <nav className="flex items-center justify-between px-6 md:px-18 lg:px-36 py-3 border-b border-gray-300 text-gray-700">
+    <nav className="flex items-center justify-between font-size:40 px-6 md:px-18 lg:px-36 py-3 border-b border-gray-300 text-gray-700">
       <Image
         className="cursor-pointer bg-transparent w-40 md:w-42"
         width={200}
